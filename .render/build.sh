01921c471ca646d6de40eb3df95daaa2de5ba110
Dhruvil Patel <dhruvil1395@gmail.com>
2025-04-10 22:42:19 +0530

    corrected the start.sh

--- .render/build.sh
+++ .render/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Install libsodium first
+sudo apt-get update
+sudo apt-get install -y libsodium-dev
+
+# Then compile
 cd server
-gcc -Wall -Wextra server.c database.c auth.c -o server -lsqlite3 -lpthread
+gcc -Wall -Wextra server.c database.c auth.c -o server -lsqlite3 -lpthread -lsodium
 chmod +x server
